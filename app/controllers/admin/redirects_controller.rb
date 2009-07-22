77e43f24e96a5e396ff9b9fe66736263b0f1f30e
Eliot Sykes <eliotsykes@gmail.com>
2009-07-22 14:20:08 +0100

    Extension now handles editing and creation of all Target types

--- app/controllers/admin/redirects_controller.rb
+++ app/controllers/admin/redirects_controller.rb
@@ -1,32 +1,41 @@
 class Admin::RedirectsController < Admin::BaseController
   resource_controller
   
-  new_action.after do
-    @target = UrlTarget.new
-    #@product_target = ProductTarget.new
-    #@taxon_target = TaxonTarget.new
-  end
-
   create.before do
-    @target = UrlTarget.new params[:target]
-    #@product_target = ProductTarget.new(params[:product_target])
-    #@taxon_target = TaxonTarget.new(params[:taxon_target])
-    @redirect.target = @target
-  end
-  create.wants.html do 
-    redirect_to collection_url
+    populate_redirect_with_target
   end
   
+  create.failure.after do
+    populate_form_models
+  end
+  
+  create.wants.html { redirect_to collection_url }
+  
   edit.before do
-    @target = @redirect.target
+    populate_form_models
   end
   
   update.before do
-    @redirect.target.update_attributes params[:target]
+    populate_redirect_with_target
   end
+  
+  update.failure.after do
+    populate_form_models
+  end
+  
   update.wants.html { redirect_to collection_url }
   
   private
+  def populate_redirect_with_target
+    @redirect.target = Target.find_or_initialize params
+  end
+  
+  def populate_form_models
+    @target = @redirect.target
+    @product = @target.product if @target.kind_of? ProductTarget
+    @taxon = @target.taxon if @target.kind_of? TaxonTarget    
+  end
+  
   #Thanks to Admin::UsersController in spree-core for initial code
   def collection   
     @search = Redirect.new_search(params[:search])
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,7 +1,20 @@
 class Target < ActiveRecord::Base
   has_many :redirects
   
+  def self.find_or_initialize(attributes)
+    type = attributes[:target][:type].constantize
+    if ProductTarget == type
+      return type.find_or_initialize_by_product_id attributes[:product][:id]
+    elsif TaxonTarget == type
+      return type.find_or_initialize_by_taxon_id attributes[:taxon][:id]
+    elsif UrlTarget == type
+      return type.find_or_initialize_by_url attributes[:target][:url]
+    end
+    raise "Unable to handle target type '#{type}'"
+  end
+  
   def type_humanized
     type.to_s.sub(/Target$/, '')
   end
+  
 end
